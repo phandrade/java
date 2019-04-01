045ced82979ff2aba207c6e10c792fb5208c2fc4
Paulo Andrade <paulo.andrade@sysmap.com.br>
2019-04-01 11:52:23 -0300

    include Serializable on input output objects

--- cast/assignment1/src/main/java/com/castgroup/assignment1/model/InputObject.java
+++ cast/assignment1/src/main/java/com/castgroup/assignment1/model/InputObject.java
@@ -1,6 +1,10 @@
 package com.castgroup.assignment1.model;
 
-public class InputObject {
+import java.io.Serializable;
+
+public class InputObject implements Serializable {
+	
+	private static final long serialVersionUID = 5630761338743937228L;
 	
 	private String data;
 
--- cast/assignment1/src/main/java/com/castgroup/assignment1/model/OutputObject.java
+++ cast/assignment1/src/main/java/com/castgroup/assignment1/model/OutputObject.java
@@ -1,7 +1,11 @@
 package com.castgroup.assignment1.model;
 
-public class OutputObject {
+import java.io.Serializable;
 
+public class OutputObject implements Serializable {
+
+	private static final long serialVersionUID = 7726555731613868779L;
+	
 	private String erro;
 	private String mensagem;
 	private String data;
